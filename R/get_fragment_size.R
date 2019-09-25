bf0076075ab89c81364dc3fee5ad7e940ba41ade
amjad <amjad.alkodsi@gmail.com>
2019-09-25 12:26:50 +0000

    added fragment length functions

---
+++ R/get_fragment_size.R
@@ -0,0 +1,100 @@
+#' Get fragment lengths from bam
+#'
+#' A function to extract fragment lengths from a bam file. Optionally, given a mutation data frame, it can categorize read lengths 
+#' in mutated vs non-mutated reads.
+#' @param bam path to bam file
+#' @param mutations An optional data frame with mutations. Must have the columns CHROM, POS, REF, ALT.
+#' @param tag the RG tag if the bam has more than one samplee
+#' @param isProperPair a logical wheter to return only proper pairs (T), only improper pairs (F), or it does not matter (NA)
+#' @param low_bound Integer with the lowest fragment length
+#' @param high_bound Integer with the highest fragment length
+#' @return A data frame with the columns Sample (SM tag in bam), ID (read ID), size (fragment size), and category (only if mutations is provided)
+#' @export 
+
+get_fragment_size <- function(bam, mutations = NULL, tag = "", isProperPair = NA, low_bound = 1, high_bound = 400) {
+
+    assertthat::assert_that(!missing(bam), is.character(bam), length(bam) == 1, file.exists(bam))
+
+    assertthat::assert_that(is.character(tag), length(tag) == 1)
+    
+    assertthat::assert_that(is.logical(isProperPair), is.numeric(low_bound), is.numeric(high_bound),
+    	length(isProperPair) == 1, length(low_bound) == 1, length(high_bound) == 1)
+
+    if(!is.null(mutations)) {
+        
+        assertthat::assert_that(is.data.frame(mutations), assertthat::not_empty(mutations), 
+        
+        assertthat::has_name(mutations, c("CHROM", "POS", "REF", "ALT")))
+
+        assertthat::assert_that(all(nchar(mutations$REF) == 1),
+            all(nchar(mutations$ALT) == 1),
+            msg = "Only SNVs are supported") 
+    
+        assertthat::assert_that(is.character(mutations$REF), is.character(mutations$ALT),
+            all(mutations$REF %in% c("A", "C", "T", "G")), all(mutations$ALT %in% c("A", "C", "T", "G")),
+            msg = "REF and ALT in mutations should be characters having basepairs")
+
+        assertthat::assert_that(is.numeric(mutations$POS), all(mutations$POS > 0))
+
+        assertthat::assert_that(all(mutations$CHROM %in% get_bam_chr(bam)))
+    }
+
+    flag <- Rsamtools::scanBamFlag(isPaired = T, isProperPair = isProperPair, isUnmappedQuery = F,
+    	      hasUnmappedMate = F, isFirstMateRead = T, isSecondaryAlignment = F,
+    	      isNotPassingQualityControls = F, isDuplicate = F, isSupplementaryAlignment = F)
+    
+    if(tag != ""){
+	
+	    assertthat::assert_that(verify_tag(bam = bam, tag = tag), 
+           msg = "Specified tag not found")
+	    
+	    sbp <- Rsamtools::ScanBamParam(flag = flag, 
+	       tagFilter = list(RG = tag), what = Rsamtools::scanBamWhat())
+    
+    } else {
+
+        sbp <- Rsamtools::ScanBamParam(flag = flag, what = Rsamtools::scanBamWhat())
+    
+    }
+    
+    sm <- get_bam_SM(bam = bam, tag = tag)
+    
+    scanned_bam <- Rsamtools::scanBam(bam, param = sbp)[[1]]
+   
+    fragment_lengths <- data.frame(Sample = sm, 
+    	ID = paste(sm, scanned_bam$qname, sep = "_"), 
+    	size = abs(scanned_bam$isize),
+    	stringsAsFactors = F) %>%
+      dplyr::filter(size >= low_bound & size <= high_bound)
+
+    if(!is.null(mutations)) {
+     
+        read_names <- unique(unlist(get_mutations_read_names(bam = bam, tag = tag, mutations = mutations)))
+        
+        fragment_lengths <- dplyr::mutate(fragment_lengths, 
+        	category = ifelse(ID %in% read_names, "mutated", "other"))
+    }
+
+    return(fragment_lengths)
+}
+
+#' Plot density of variable
+#' @param df data frame in long format
+#' @param var the column of which to plot the histogram
+#' @param color the column containing the categories
+#' @param binwidth bin width of the histogram
+#' @return a ggplot object
+#' @export
+
+plot_density <- function(df, var = "size", color = "Sample", binwidth = 2.5) {
+ 
+    ggplot2::ggplot() + 
+    ggplot2::geom_freqpoly(data = df, 
+  	   ggplot2::aes_string(x = "size", y = "..density..", color = color), 
+  	   binwidth = binwidth) +
+    ggplot2::theme_minimal()
+
+}
+
+
+
---
+++ R/get_mutations_read_names.R
@@ -0,0 +1,89 @@
+#' Get names of the reads showing alternative allele of a mutation
+#'
+#' Extract the names of the reads in a bam file that support the variant allele of a single mutation
+#' @param bam path to bam file
+#' @param chr Chromosome name for the mutation
+#' @param pos Chromosome position for the mutation
+#' @param alt The alternative allele of the mutation
+#' @param tag the RG tag if the bam has more than one samplee
+#' @return A character vector having the read names
+#' @export 
+
+get_mutation_read_names <- function(bam, chr, pos, alt, tag = "") {
+
+    assertthat::assert_that(!missing(bam), is.character(bam), length(bam) == 1, file.exists(bam))
+
+    assertthat::assert_that(is.character(tag), length(tag) == 1)
+
+    assertthat::assert_that(is.numeric(pos), pos > 0, pos %% 1 == 0, length(pos) == 1)
+
+    assertthat::assert_that(is.character(chr), length(chr) == 1, chr %in% get_bam_chr(bam))
+
+    assertthat::assert_that(is.character(alt), length(alt) == 1, alt %in% c("C","T","G","A"))
+
+    gr <- GenomicRanges::GRanges(chr, IRanges::IRanges(pos, pos))
+  
+    if(tag == ""){
+   
+      stackedStrings <- GenomicAlignments::stackStringsFromBam(bam, use.names = T, param = gr)
+  
+    } else {
+  
+  	  assertthat::assert_that(verify_tag(bam = bam, tag = tag), 
+          msg = "Specified tag not found")
+
+      stackedStrings <- GenomicAlignments::stackStringsFromBam(bam, use.names = T, 
+          param = Rsamtools::ScanBamParam(tagFilter = list("RG" = tag), which = gr)) 
+  
+    }
+
+    sm <- get_bam_SM(bam = bam, tag = tag)
+
+    out <- data.frame(ID = paste(sm, names(stackedStrings), sep = "_"), 
+  	  seq = as.data.frame(stackedStrings)[,1])
+  
+    return(unique(as.character(out[out$seq == alt, "ID"])))
+}
+
+
+#' Get names of the reads showing alternative allele of a mutation
+#'
+#' Extract the names of the reads in a bam file that support the variant allele of a single mutation
+#' @param bam path to bam file
+#' @param mutations A data frame containing the mutations. Must have the columns CHROM, POS, REF, ALT.
+#' @param tag the RG tag if the bam has more than one samplee
+#' @return A list with length equal to the number of mutations. Each element is a character vector with the read names.
+#' @export 
+
+get_mutations_read_names <- function(bam, mutations, tag = "") {
+  
+    assertthat::assert_that(!missing(bam), is.character(bam), 
+      length(bam) == 1, file.exists(bam))
+
+    assertthat::assert_that(!missing(mutations), is.data.frame(mutations), assertthat::not_empty(mutations), 
+        assertthat::has_name(mutations, c("CHROM", "POS", "REF", "ALT")))
+
+    assertthat::assert_that(all(nchar(mutations$REF) == 1),
+        all(nchar(mutations$ALT) == 1),
+        msg = "Only SNVs are supported") 
+    
+    assertthat::assert_that(is.character(mutations$REF), is.character(mutations$ALT),
+        all(mutations$REF %in% c("A", "C", "T", "G")), all(mutations$ALT %in% c("A", "C", "T", "G")),
+        msg = "REF and ALT in mutations should be characters having basepairs")
+
+    assertthat::assert_that(is.numeric(mutations$POS), all(mutations$POS > 0))
+
+    assertthat::assert_that(all(mutations$CHROM %in% get_bam_chr(bam)))
+
+    read_names <- purrr::pmap(list(mutations$CHROM, mutations$POS, mutations$ALT),
+        function(chr, pos, alt) {
+            counts <- get_mutation_read_names(bam = bam, tag = tag,
+              chr = chr, pos = pos, alt = alt)
+        })
+    
+    names(read_names) <- paste0(mutations$CHROM, ":", mutations$POS, "_", 
+        mutations$REF, "_", mutations$ALT)
+    
+    return(read_names)
+
+}
