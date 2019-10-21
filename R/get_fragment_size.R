1412dc5ab470c549f13235bd2180a4a96a8faf95
amjad <amjad.alkodsi@gmail.com>
2019-10-21 10:44:48 +0000

    added visualization functions

--- R/get_fragment_size.R
+++ R/get_fragment_size.R
@@ -58,12 +58,12 @@ get_fragment_size <- function(bam, mutations = NULL, tag = "", isProperPair = NA
            msg = "Specified tag not found")
 	    
 	    sbp <- Rsamtools::ScanBamParam(flag = flag, mapqFilter = 30, simpleCigar = simple_cigar,
-	       tagFilter = list(RG = tag), what = c("qname","flag","qwidth","isize"))
+	       tagFilter = list(RG = tag), what = c("qname","flag","qwidth","isize","rname","pos","mpos"))
     
     } else {
 
         sbp <- Rsamtools::ScanBamParam(flag = flag, mapqFilter = 30, simpleCigar = simple_cigar,
-           what = c("qname","flag","qwidth","isize"))
+           what = c("qname","flag","qwidth","isize","rname","pos","mpos"))
     
     }
     
@@ -90,6 +90,9 @@ get_fragment_size <- function(bam, mutations = NULL, tag = "", isProperPair = NA
     fragment_lengths <- data.frame(Sample = sm, 
     	ID = paste(sm, scanned_bam$qname, sep = "_"),
     	size = abs(scanned_bam$isize),
+        chr = scanned_bam$rname,
+        pos = scanned_bam$pos,
+        mpos = scanned_bam$mpos,
     	stringsAsFactors = F) %>%
       dplyr::filter(.data$size >= min_size & .data$size <= max_size)
 
@@ -103,3 +106,104 @@ get_fragment_size <- function(bam, mutations = NULL, tag = "", isProperPair = NA
 
     return(fragment_lengths)
 }
+
+#' @export
+plot_fragment_size_region <- function(fragments1, fragments2 = NULL, chr, start, end, 
+    smoothing_window = 100, genome = c("hg19","hg38")) {
+    
+    fragments1_bychr <- split(fragments1, fragments1$chr)
+    
+    fragments1 <- purrr::map_dfr(fragments1_bychr, 
+        ~ dplyr::mutate(.x, smoothed_size = RcppRoll::roll_mean(.data$size, n = smoothing_window, fill = 0))) %>%
+       dplyr::filter(!is.na(.data$smoothed_size))
+    
+    sample_levels <- unique(fragments1$Sample)
+
+    if(!is.null(fragments2)){
+        fragments2_bychr <- split(fragments2, fragments2$chr)
+         
+        fragments2<- purrr::map_dfr(fragments2_bychr, 
+             ~ dplyr::mutate(.x, smoothed_size = RcppRoll::roll_mean(.data$size, n = smoothing_window, fill = 0))) %>%
+               dplyr::filter(!is.na(.data$smoothed_size))
+        
+        sample_levels <- c(sample_levels, unique(fragments2$Sample))
+    
+    }
+    
+    genome <- match.arg(genome)
+
+    gtrack <- Gviz::GenomeAxisTrack()
+
+    itrack <- Gviz::IdeogramTrack( chromosome=chr, genome = genome)
+    
+
+    data1 <- GenomicRanges::GRanges(fragments1$chr, IRanges(fragments1$pos, fragments1$pos), size = fragments1$smoothed_size)
+
+    covTrack1 <- Gviz::DataTrack(as(coverage(data1), "GRanges"), genome = genome,
+         type = "l", name = "Coverage", groups=factor(unique(fragments1$Sample), levels=sample_levels))
+
+    dtrack1 <- Gviz::DataTrack(data1,  genome = genome, name = "Fragment Length",
+         groups=factor(unique(fragments1$Sample), levels=sample_levels))
+    
+    if(!is.null(fragments2)){
+   
+        data2 <- GenomicRanges::GRanges(fragments2$chr, IRanges(fragments2$pos, fragments2$pos), size2 = fragments2$smoothed_size)
+        
+        covTrack2 <- DataTrack(as(coverage(data2), "GRanges"), genome = genome,
+           type = "l", name = "Coverage", groups=factor(unique(fragments2$Sample), levels=sample_levels))
+        
+        dtrack2 <- Gviz::DataTrack(data2, groups=factor(unique(fragments2$Sample), levels=sample_levels),
+             genome = genome, name = "Fragment Length")
+
+        overlay <- Gviz::OverlayTrack(trackList = list(dtrack1, dtrack2))
+   
+        overlay2 <- Gviz::OverlayTrack(trackList = list(covTrack1, covTrack2))
+        #ylims <- extendrange(range(c(values(dtrack1), values(dtrack2))))
+   
+        Gviz::plotTracks(list(itrack, gtrack, overlay2, overlay), from = start, to = end, chromosome = chr,  type="a",
+            background.panel="#FFFEDB", background.title="darkblue")
+   
+    } else {
+
+        Gviz::plotTracks(list(itrack, gtrack, covTrack1, dtrack1), from = start, to = end, chromosome = chr, type="a",
+            background.panel="#FFFEDB", background.title="darkblue")
+
+    }
+
+}
+
+#' @export
+summarize_fragment_size <- function(bam, regions, tag = "", ...){
+   
+    assertthat::assert_that(assertthat::has_name(regions, c("chr", "start", "end")),
+        is.data.frame(regions))
+
+    fragments <- get_fragment_size(bam = bam, tag = tag, ...)
+
+    regions$ID <- paste0(regions$chr, ":", regions$start, "-", regions$end)
+
+    regions_gr <- GenomicRanges::GRanges(regions$chr, IRanges::IRanges(regions$start, regions$end))
+
+    fragments_gr <- GenomicRanges::GRanges(fragments$chr, IRanges::IRanges(fragments$pos, fragments$pos))
+
+    overlaps <- as.data.frame(GenomicRanges::findOverlaps(fragments_gr, regions_gr))
+
+    fragments$Region <- NA
+
+    fragments[overlaps[,1], "Region"] <- regions[overlaps[,2], "ID"]
+   
+    summary <- fragments %>% 
+        tidyr::replace_na(list(Region = "offTargets")) %>%
+        dplyr::group_by(.data$Region) %>%
+        dplyr::summarize(mean = mean(.data$size)) %>%
+        as.data.frame()
+
+    colnames(summary) <- c("Region", unique(fragments$Sample))
+
+    return(summary)
+   
+}
+
+
+#plot_fragment_size_region(k, chr = "chr18", start = 60774470, end = 60774594)
+#plot_fragment_size_region(s25,k, chr = "chr12", start = 113515229-2000, end = 113515910+2000, smoothing_window = 200)
