e6d6144a72628c95de1fea0e9e901e36e77df38a
amjad <amjad.alkodsi@gmail.com>
2019-12-09 15:30:01 +0000

    added fragmentation functionality

---
+++ R/compute_fragment_size_wps.R
@@ -0,0 +1,71 @@
+#' Analysis of fragment ends
+#'
+#' Caluclates the number of fragment ends and the Windowed Protection Score (WPS) in genomic tiles within targets
+#'
+#' @param bam the input bam file
+#' @param targets The targets to restrict the windows within. Must have the columns chr, start and end.
+#'  In case of whole-genome, specify full chromosomes targets.
+#' @param tag  the RG tag if the bam has more than one samplee.
+#' @param window_size The window (bin) size to use within the targets
+#' @param step_size The step size to use in case of overlapping bins.
+#' @param min_size Restrict fragments to this minimum size.
+#' @param max_size Restrict fragments to this maximum size.
+#' @param ... Other parameters passed to get_fragment_size
+#' @return a data frame with the first three columns having the bins coordinates and 
+#' other columns having the WPS (raw and adjusted by coverage) and number of fragment ends (raw and adjusted by coverage).
+#' @details Check Snyder et al., Cell 2016.
+#' @export
+#' @importFrom rlang .data
+#' @importFrom magrittr %>%
+
+compute_fragment_size_wps <- function(bam, targets, tag = "", window_size = 120,
+    step_size = 5, min_size = 120, max_size = 180, ...) {
+
+    assertthat::assert_that(assertthat::has_name(targets, c("chr", "start", "end")),
+        is.data.frame(targets))
+
+    assertthat::assert_that(!missing(bam), is.character(bam), length(bam) == 1, file.exists(bam))
+
+    assertthat::assert_that(is.character(tag), length(tag) == 1)
+    
+    assertthat::assert_that(is.numeric(window_size), is.numeric(step_size),
+    	length(window_size) == 1, length(step_size) == 1)
+
+    sm <- get_bam_SM(bam = bam, tag = tag)
+
+    ellipsis::check_dots_used()
+
+    reads <- get_fragment_size(bam = bam, tag = tag, 
+    	min_size = min_size, max_size = max_size, ...)
+
+    reads_gr <- GenomicRanges::GRanges(reads$chr, IRanges::IRanges(reads$start, reads$end))
+
+    regions <-  purrr::pmap_dfr(list(targets$chr, targets$start, targets$end), 
+    	function(x, y, z) { 
+    		data.frame(chr = x, start = seq(y - window_size + 1, z + window_size - 1, by = step_size),
+    		    stringsAsFactors = F) %>%
+    	        dplyr::mutate(end = .data$start + window_size)
+    	    })
+    
+    frag_ends_gr <- GenomicRanges::GRanges(rep(reads$chr, 2),
+        IRanges::IRanges(c(reads$start, reads$end), c(reads$start, reads$end)))
+
+    regions_gr <- GenomicRanges::GRanges(regions$chr, IRanges::IRanges(regions$start, regions$end))
+
+    n_overlaps <- GenomicRanges::countOverlaps(regions_gr, reads_gr, type = "any")
+
+    n_within_overlaps <-  GenomicRanges::countOverlaps(regions_gr, reads_gr, type = "within")
+
+    n_side_overlaps <- n_overlaps - n_within_overlaps
+
+    n_frag_ends <- GenomicRanges::countOverlaps(regions_gr, frag_ends_gr)
+
+    regions <- regions %>%
+        dplyr::mutate(WPS = (n_within_overlaps - n_side_overlaps),
+            WPS_adjusted = WPS / ifelse(n_overlaps == 0, 1, n_overlaps),
+            n_fragment_ends = n_frag_ends,
+            n_fragment_ends_adjusted = n_frag_ends / ifelse(n_overlaps == 0, 1, n_overlaps))
+
+    return(regions)     
+
+}
